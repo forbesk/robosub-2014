4f79f1fb261b50797bd5543bf4bf6283bc94f534
Kevin Forbes <forbesk9@gmail.com>
2014-05-23 21:10:50 -0700

    basic task framework

--- src/auv_control/org/auvua/agent/Task.java
+++ src/auv_control/org/auvua/agent/Task.java
@@ -1,5 +1,33 @@
 package org.auvua.agent;
 
-public class Task {
+import org.auvua.model.Model;
+
+public abstract class Task implements Runnable {
+
+	public Model model = null;
+	public Task successTask = null;
+	public Task failureTask = null;
+	public Task timeoutTask = null;
+	
+	@Override
+	abstract public void run();
+	
+	abstract public void cleanup();
+	
+	public void setModel(Model m) {
+		model = m;
+	}
+	
+	public void setSuccessTask(Task t) {
+		successTask = t;
+	}
+	
+	public void setFailureTask(Task t) {
+		failureTask = t;
+	}
+	
+	public void setTimeoutTask(Task t) {
+		timeoutTask = t;
+	}	
 	
 }
