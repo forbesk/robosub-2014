53353fc6ba7c307307b8c8fe91a27fa195d5d890
Evan Briones <erbriones@gmail.com>
2014-07-19 18:58:28 -0700

    port Footer components browserify and JSX
    	modified:   dashboard/src/js/components/application.js
    	modified:   dashboard/src/js/components/footer.js

--- dashboard/src/js/components/application.js
+++ dashboard/src/js/components/application.js
@@ -2,11 +2,16 @@
 
 var React = require('react');
 var Header = require('./header');
+var Footer = require('./footer');
+var Telemetry = require('./telemetry');
 
 var Application = React.createClass({
     render: function() {
         return (
-            <Header status={"running"} />
+            <div id="container">
+                <Header status={"running"} />
+                <Footer />
+            </div>
         );
     }
 });
